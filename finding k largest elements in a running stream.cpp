5fc5888e4454ff63f4c3868184aac17ad4e897e7
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-06-19 23:30:57 +0530

    Add files via upload

---
+++ finding k largest elements in a running stream.cpp
@@ -0,0 +1,14 @@
+//Running stream means we can not store all the numbers since they are not definite and keep on comming 
+//we have to find k largest elements 
+//we will be using min heap for it
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int no;
+    while(scanf(%d,&no)!=EOF)
+    {
+        cout<<no<<",";
+    }
+}
